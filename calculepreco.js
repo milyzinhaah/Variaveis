5f73dfeb743c41912aa631a20cca956804d447e9
milyzinhaah <Amorim.hemily@escola.pr.gov.br>
2025-10-22 21:21:37 -0300

    Add files via upload

---
+++ calculepreco.js
@@ -0,0 +1,6 @@
+let precoproduto=79.90
+let desconto=0.15
+precototal=precoproduto*desconto
+precofinal=precoproduto-precototal
+
+console.log(`O preço do produto é R$: ${precoproduto}\n Preço final é R$: ${precofinal.toFixed(2)}`);
